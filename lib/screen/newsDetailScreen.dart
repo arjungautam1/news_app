61cb51a6cd5e9eeaef3c105e84cf120e8ae1fd85
arjun-sudo <laserarjun@gmail.com>
2020-10-27 23:19:44 +0545

    url launcher

--- lib/screen/newsDetailScreen.dart
+++ lib/screen/newsDetailScreen.dart
@@ -4,11 +4,29 @@
  */
 import 'package:flutter/material.dart';
 import 'package:news_api/model/news.dart';
+import 'package:social_share/social_share.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NewsDetailScreen extends StatelessWidget {
   News news;
 
   NewsDetailScreen({this.news});
+
+  getImageDetail(String imageUrl) {
+    if (imageUrl != null) {
+      return Image.network(imageUrl);
+    } else {
+      return SizedBox.shrink();
+    }
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,12 +63,24 @@ class NewsDetailScreen extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(news.description),
+              padding: const EdgeInsets.all(32.0),
+              child: getImageDetail(news.urlToImage),
             ),
             Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Image.network(news.urlToImage),
+              padding: const EdgeInsets.all(8.0),
+              child: Text(news.content),
+            ),
+            Center(
+              child: RaisedButton(
+                onPressed: () {
+                  _launchURL(news.url);
+                },
+                color: Colors.blueAccent,
+                child: Text(
+                  'Read full News',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
             ),
           ],
         ),
